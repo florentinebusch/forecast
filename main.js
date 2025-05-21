3d5b6ad2c37cb7f867fad603328ddfcc623ce09a
florentinebusch <florentine.busch@student.uibc.ac.at>
2025-05-21 22:59:11 +0200

    Wettersymbole für die nächsten 24 Stunden in 3 Stunden Abständen

--- main.js
+++ main.js
@@ -58,7 +58,14 @@ let markup = `
 </ul>
   `;
 
-   L.popup([latlng.lat, latlng.lng], {
+// Wettericons für die nächsten 24 Stunden in 3 Stunden Schritten
+for (let i=0; i <=24; i += 3) {
+    let symbol = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+     
+    markup += `<img src="icons/${symbol}.svg" style="width:32px">`;
+    }
+
+L.popup([latlng.lat, latlng.lng], {
     content: markup
 }).openOn(overlays.forecast);
 
